a44cd75d6cb7c14474e58670e180d8c40b0ef411
Arri <arriye_@hotmail.com>
2018-02-17 23:59:40 -0500

    fixed test crashing code sandbox

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<div>
+    <BrowserRouter>
+    <MuiThemeProvider theme={theme}>
+    <App />
+    </MuiThemeProvider>
+    </BrowserRouter>
+   </div>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
